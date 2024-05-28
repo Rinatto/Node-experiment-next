8fed8e3ded230309f0886fee43f22868957a3471
Apple <apple@MacBook-Pro-Apple.local>
2024-05-28 23:45:37 +0300

    Prepare for Netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        './lib/runExperiments': 'commonjs ./lib/runExperiments',
+        './lib/runExperimentsHandler': 'commonjs ./lib/runExperimentsHandler',
+        './lib/js/testDynamicStructureChange': 'commonjs ./lib/js/testDynamicStructureChange',
+        './lib/ts/testDynamicStructureChange': 'commonjs ./lib/ts/testDynamicStructureChange'
+      });
+    }
+    return config;
+  }
+};
+
+export default nextConfig;
