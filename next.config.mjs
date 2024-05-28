f215c22e784499cc50d9aa8451f721485b9857ad
Apple <apple@MacBook-Pro-Apple.local>
2024-05-29 00:25:51 +0300

    change .toml

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,12 @@
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push({
-        './lib/runExperiments': 'commonjs ./lib/runExperiments',
-        './lib/runExperimentsHandler': 'commonjs ./lib/runExperimentsHandler',
-        './lib/js/testDynamicStructureChange': 'commonjs ./lib/js/testDynamicStructureChange',
-        './lib/ts/testDynamicStructureChange': 'commonjs ./lib/ts/testDynamicStructureChange'
-      });
-    }
-    return config;
-  }
-};
-
-export default nextConfig;
+    webpack: (config, { dev, isServer }) => {
+      if (dev) {
+        config.devtool = 'cheap-module-source-map';
+      }
+      return config;
+    },
+    reactStrictMode: true,
+  };
+  
+  export default nextConfig;
